c194efb8cb65a9a9633c1d1af35ea2d8161698e1
Tetiana Turchyn <tetiana.turchyn@grammarly.com>
2017-04-14 16:35:00 +0300

    Add ProblemSet0

---
+++ ProblemSet0.py
@@ -0,0 +1,14 @@
+#ProblemSet0
+#by tetiana Turchyn
+
+"""
+Ask the user to enter two numbers: x and y.
+Output x to the power y and log2(x)
+"""
+
+import numpy
+
+x = int(raw_input("Enter the first number: "))
+y = int(raw_input("Enter the second number: "))
+print "X =",x,"to the power Y =",y,"is: ", x**y
+print "log(x) = ", numpy.log2(x)
